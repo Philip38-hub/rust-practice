5610c84c8f0223c117997dceecfd21194956f1ca
philip <oumaphilip01@gmail.com>
2025-04-02 11:23:11 +0300

    add temperature_conv

---
+++ temperature_conv/src/lib.rs
@@ -0,0 +1,18 @@
+pub fn fahrenheit_to_celsius(f: f64) -> f64 {
+    (f-32.0) * 5.0/9.0
+}
+
+pub fn celsius_to_fahrenheit(c: f64) -> f64 {
+    9.0/5.0 * c + 32.0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = celsius_to_fahrenheit(0.0);
+        assert_eq!(result, 32.0);
+    }
+}
---
+++ temperature_conv/target/debug/deps/temperature_conv-91ad9cc1f8324a7c.d
@@ -0,0 +1,5 @@
+/home/pochieng/Desktop/piscine-rust/temperature_conv/target/debug/deps/temperature_conv-91ad9cc1f8324a7c: src/lib.rs
+
+/home/pochieng/Desktop/piscine-rust/temperature_conv/target/debug/deps/temperature_conv-91ad9cc1f8324a7c.d: src/lib.rs
+
+src/lib.rs:
