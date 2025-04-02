192a690bf3222a16ba1e66e4909d09daaf9b5551
philip <oumaphilip01@gmail.com>
2025-04-02 11:46:55 +0300

    Updated .gitignore to properly exclude files

--- temperature_conv/target/debug/deps/temperature_conv-4eb04a7507869acf.d
+++
@@ -1,7 +0,0 @@
-/home/pochieng/Desktop/piscine-rust/temperature_conv/target/debug/deps/libtemperature_conv-4eb04a7507869acf.rmeta: src/lib.rs
-
-/home/pochieng/Desktop/piscine-rust/temperature_conv/target/debug/deps/libtemperature_conv-4eb04a7507869acf.rlib: src/lib.rs
-
-/home/pochieng/Desktop/piscine-rust/temperature_conv/target/debug/deps/temperature_conv-4eb04a7507869acf.d: src/lib.rs
-
-src/lib.rs:
