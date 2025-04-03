95871f1e6a6809722f94e7da464eca816a50d0c8
Philip <oumaphilip01@gmail.com>
2025-04-03 23:54:43 +0300

    add copy

---
+++ copy/src/lib.rs
@@ -0,0 +1,24 @@
+pub fn nbr_function(c: i32) -> (i32, f64, f64) {
+    let exponential = (c as f64).exp();
+    let log = (c as f64).ln();
+    (c, exponential, log)
+}
+
+pub fn str_function(a: String) -> (String, String) {
+    let exponential = a
+        .split_whitespace()
+        .filter_map(|num| num.parse::<f64>().ok())
+        .map(|num| num.exp().to_string())
+        .collect::<Vec<string>>();
+        .join(" ");
+    
+    (a, exponential)
+}
+
+pub fn vec_function(b: Vec<i32>) -> (Vec<i32>, Vec<f64>) {
+    let log = b
+        .iter()
+        .map(|&num| (num.abs() as f64).ln())
+        .collect::<Vec<f64>>();
+    (b, log)
+}
