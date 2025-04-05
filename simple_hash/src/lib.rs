1ff7168edfd0a3fdfd539089205495e6e8784e69
Philip <oumaphilip01@gmail.com>
2025-04-05 10:37:04 +0300

    update hashmap

--- simple_hash/src/lib.rs
+++ simple_hash/src/lib.rs
@@ -1,12 +1,10 @@
 use std::collections::HashMap;
 
-pub fn word_frequency_counter<'a>(words: &'a Vec<&'a str>) -> HashMap<&'a str, usize> {
+pub fn word_frequency_counter<'a>(words: &'a [&'a str]) -> HashMap<&'a str, usize> {
     let mut map = HashMap::new();
 
     for &word in words {
-        // Remove trailing punctuation like "." or "," using trim_end_matches
         let cleaned = word.trim_end_matches(|c: char| !c.is_alphanumeric());
-
         *map.entry(cleaned).or_insert(0) += 1;
     }
 
