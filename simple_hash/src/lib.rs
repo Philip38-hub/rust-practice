1e135cec14e3397ef86e3752f9887d5f1d928cf6
Philip <oumaphilip01@gmail.com>
2025-04-05 10:40:23 +0300

    update

--- simple_hash/src/lib.rs
+++ simple_hash/src/lib.rs
@@ -1,14 +1,12 @@
 use std::collections::HashMap;
 
 pub fn word_frequency_counter<'a>(words: &'a [&'a str]) -> HashMap<&'a str, usize> {
-    let mut map = HashMap::new();
-
-    for &word in words {
-        let cleaned = word.trim_end_matches(|c: char| !c.is_alphanumeric());
-        *map.entry(cleaned).or_insert(0) += 1;
+    let mut char_count = HashMap::new();
+    for word in words {
+        let count = char_count.entry(*word).or_insert(0);
+        *count += 1;
     }
-
-    map
+    char_count
 }
 
 pub fn nb_distinct_words(frequency_count: &HashMap<&str, usize>) -> usize {
