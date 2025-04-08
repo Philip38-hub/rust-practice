cc4cff3adb028735ec6880800d89c46def747ef3
philip <oumaphilip01@gmail.com>
2025-04-08 11:29:48 +0300

    update unwrap or expect

--- unwrap_or_expect/src/lib.rs
+++ unwrap_or_expect/src/lib.rs
@@ -9,7 +9,7 @@ pub enum Security {
 pub fn fetch_data(server: Result<&str, &str>, security_level: Security) -> String {
     match security_level {
         Security::Unknown => server.unwrap().to_string(),
-        Security::Mesage => server.expect("ERROR: program stops").to_string(),
+        Security::Message => server.expect("ERROR: program stops").to_string(),
         Security::Warning => server.unwrap_or("WARNING: check the server").to_string(),
         Security::NotFound => match server {
             Ok(url) => url.to_string(),
@@ -17,7 +17,7 @@ pub fn fetch_data(server: Result<&str, &str>, security_level: Security) -> Strin
         },
         Security::UnexpectedUrl => match server {
             Ok(url) => panic!("{}", url),
-            Err(msg) => err.to_string(),
+            Err(msg) => Err.to_string(),
         }
     }
 }
