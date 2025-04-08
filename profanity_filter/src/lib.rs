99c12c76961a7caaf816e150e139f7e10508d479
philip <oumaphilip01@gmail.com>
2025-04-08 13:26:44 +0300

    update profanity filter

--- profanity_filter/src/lib.rs
+++ profanity_filter/src/lib.rs
@@ -1,26 +1,26 @@
-pub struct Message {
-    content: String,
-    user: String,
+pub struct Message<'a> {
+    content: &'a str,
+    user: &'a str,
 }
 
-impl Message {
-    pub fn new(content: String, user: String) -> Self {
+impl<'a> Message<'a> {
+    pub fn new(content: &'a str, user: &'a str) -> Self {
         Self { content, user }
     }
 
-    pub fn send_ms(&self) -> Option<String> {
+    pub fn send_ms(&self) -> Option<&'a str> {
         if self.content.is_empty() || self.content.contains("stupid") {
             None
         } else {
-            Some(self.content.clone())
+            Some(self.content)
         }
     }
 }
 
-pub fn check_ms(message: &str) -> Result<String, String> {
-    let msg = Message::new(message.to_string(), "anonymous".to_string());
+pub fn check_ms(message: &str) -> Result<&str, &str> {
+    let msg = Message::new(message, "anonymous");
     match msg.send_ms() {
         Some(valid_msg) => Ok(valid_msg),
-        None => Err("ERROR: illegal".to_string()),
+        None => Err("ERROR: illegal"),
     }
 }
