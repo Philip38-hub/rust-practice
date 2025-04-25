42d2b28cc425e6e87c1bb234dd73238ece4e4f68
Philip <oumaphilip01@gmail.com>
2025-04-25 12:21:45 +0300

    update generated reciept to return in order

--- sales/src/lib.rs
+++ sales/src/lib.rs
@@ -33,31 +33,26 @@ impl Cart {
         let mut prices: Vec<f32> = self.items.iter().map(|(_, price)| *price).collect();
         let mut receipt: Vec<f32> = vec![0.0; prices.len()];
         let mut total_discount = 0.0;
-
-        // Process each group of 3 items
+    
         let mut i = 0;
         while i + 2 < prices.len() {
-            let group = &mut prices[i..i + 3];
             let mut indices = vec![i, i + 1, i + 2];
             indices.sort_by(|&a, &b| prices[a].partial_cmp(&prices[b]).unwrap());
-
             let cheapest_index = indices[0];
-            let discount = prices[cheapest_index];
-            total_discount += discount;
-
+            total_discount += prices[cheapest_index];
             i += 3;
         }
-
-        // Distribute the total discount proportionally
+    
         let sum: f32 = prices.iter().sum();
         for (i, &price) in prices.iter().enumerate() {
             let proportion = price / sum;
             let adjusted_price = price - proportion * total_discount;
-            // Round to two decimal places
             receipt[i] = (adjusted_price * 100.0).round() / 100.0;
         }
-
+    
+        receipt.sort_by(|a, b| a.partial_cmp(b).unwrap()); // 👈 Sort the receipt
         self.receipt = receipt.clone();
         receipt
     }
+    
 }
